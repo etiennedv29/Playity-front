1ab3ec74e01506516e4b8e03631092caaf618161
Etienne de V <edevalmont@gmail.com>
2025-05-20 14:59:53 +0200

    conflict resolutions

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-<<<<<<< HEAD
-    domains: ["cdn.pixabay.com"],
-=======
     domains: ["cdn.pixabay.com","api.dicebear.com"],
->>>>>>> a140d1fab894f4665732acf08ff61fc7752de719
   },
 };
 
