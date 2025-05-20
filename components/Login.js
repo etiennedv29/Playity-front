fadb61348b4a2458d3175bf2c3384af3f1892d0c
Etienne de V <edevalmont@gmail.com>
2025-05-20 18:06:09 +0200

    signup with google

--- components/Login.js
+++ components/Login.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { login } from "../reducers/users";
-
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
@@ -31,9 +32,9 @@ function Login() {
 
   // User connection
 
-  async function handleSignin() {
+  async function handleSignin(email, password, connectionWithSocials = false) {
     console.log("want to signin");
-    if (password === "" || email === "") {
+    if ((!connectionWithSocials && password === "") || email === "") {
       setMissingFields(true);
       return;
     } else {
@@ -41,7 +42,7 @@ function Login() {
     }
 
     //abort signin if password is not satisfying regex
-    if (!passwordRegex.test(password)) {
+    if (!connectionWithSocials && !passwordRegex.test(password)) {
       return;
     }
 
@@ -51,7 +52,7 @@ function Login() {
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, password, connectionWithSocials }),
       }
     );
     const data = await response.json();
@@ -71,7 +72,7 @@ function Login() {
         setEmail("");
         setCorrectCredentials(true);
         dispatch(rememberOrigin(""));
-        router.push("/")
+        router.push("/");
       } else if (response.status === 401) {
         setCorrectCredentials(false);
       }
@@ -82,14 +83,22 @@ function Login() {
 
   // WARNING - adapter à playity
   //Create account
-  async function handleSignup() {
+  async function handleSignup(
+    firstName,
+    lastName,
+    username,
+    password,
+    email,
+    connectionWithSocials = false
+  ) {
     console.log("signup try");
     //missing fields verification
+
     if (
       firstName === "" ||
       lastName === "" ||
       username === "" ||
-      password === "" ||
+      (!connectionWithSocials && password === "") ||
       email === ""
     ) {
       console.log("missing fields");
@@ -100,12 +109,12 @@ function Login() {
     }
 
     //abort signup process if password is not satisfying regex
-    if (!passwordRegex.test(password)) {
+    if (!connectionWithSocials && passwordRegex.test(password)) {
       return;
     }
 
     //CGU checkbox verification
-    if (!isCheckedCGU) {
+    if (!connectionWithSocials && !isCheckedCGU) {
       setDisplayWarningCGU(true);
       return;
     } else {
@@ -125,6 +134,7 @@ function Login() {
           email,
           username,
           password,
+          connectionWithSocials,
         }),
       }
     );
@@ -148,6 +158,7 @@ function Login() {
         setFirstName("");
         setLastName("");
         setEmail("");
+        router.push("/");
       } else if (response.status === 409) {
         setExistingUser(true);
       }
@@ -163,7 +174,7 @@ function Login() {
 
   //signin and signup box design
   let boxSize = {
-    height: isSignupDisplay ? 550 : 300,
+    height: isSignupDisplay ? 580 : 410,
     transition: "height 0.15s ease-out",
   };
 
@@ -195,122 +206,168 @@ function Login() {
             {missingFields && (
               <p style={{ color: "lightcoral" }}>Remplis tous les champs stp</p>
             )}
-            <div className={styles.inputFieldsArea}>
-              <input
-                className={styles.loginField}
-                type="text"
-                placeholder="Prénom"
-                id="signUpfirstName"
-                onChange={(e) => setFirstName(e.target.value)}
-                value={firstName}
-              />
-              <input
-                className={styles.loginField}
-                type="text"
-                placeholder="Nom"
-                id="signUpLastName"
-                onChange={(e) => setLastName(e.target.value)}
-                value={lastName}
-              />
-              <input
-                className={styles.loginField}
-                type="text"
-                placeholder="Email"
-                id="signUpEmail"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <input
-                className={styles.loginField}
-                type="text"
-                placeholder="Pseudo"
-                id="signUpUsername"
-                onChange={(e) => setUsername(e.target.value)}
-                value={username}
-              />
-            </div>
-            <div className={styles.passwordArea}>
-              <div className={styles.passwordAreaInput}>
+            <div className={styles.traditionalSigninContainer}>
+              <div className={styles.inputFieldsArea}>
                 <input
-                  className={styles.loginFieldPassword}
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Mot de passe"
-                  id="signUpPassword"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                    setIncorrectPassword(passwordRegex.test(e.target.value));
-                    console.log(incorrectPassword);
-                  }}
-                  value={password}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleSignup();
-                    }
-                  }}
+                  className={styles.loginField}
+                  type="text"
+                  placeholder="Prénom"
+                  id="signUpfirstName"
+                  onChange={(e) => setFirstName(e.target.value)}
+                  value={firstName}
                 />
+                <input
+                  className={styles.loginField}
+                  type="text"
+                  placeholder="Nom"
+                  id="signUpLastName"
+                  onChange={(e) => setLastName(e.target.value)}
+                  value={lastName}
+                />
+                <input
+                  className={styles.loginField}
+                  type="text"
+                  placeholder="Email"
+                  id="signUpEmail"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+                <input
+                  className={styles.loginField}
+                  type="text"
+                  placeholder="Pseudo"
+                  id="signUpUsername"
+                  onChange={(e) => setUsername(e.target.value)}
+                  value={username}
+                />
+              </div>
+              <div className={styles.passwordArea}>
+                <div className={styles.passwordAreaInput}>
+                  <input
+                    className={styles.loginFieldPassword}
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Mot de passe"
+                    id="signUpPassword"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                      setIncorrectPassword(passwordRegex.test(e.target.value));
+                      console.log(incorrectPassword);
+                    }}
+                    value={password}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        handleSignup(
+                          firstName,
+                          lastName,
+                          username,
+                          password,
+                          email,
+                          (connectionWithSocials = false)
+                        );
+                      }
+                    }}
+                  />
+                  <div
+                    onClick={() => handleShowPassword()}
+                    className={styles.toggleShowPassword}
+                  >
+                    {!showPassword ? (
+                      <FontAwesomeIcon
+                        icon={faEye}
+                        color="#F5C242"
+                        size="lg"
+                        style={{ cursor: "pointer" }}
+                      />
+                    ) : (
+                      <FontAwesomeIcon
+                        icon={faEyeSlash}
+                        color="#F5C242"
+                        size="lg"
+                        style={{ cursor: "pointer" }}
+                      />
+                    )}
+                  </div>
+                </div>
                 <div
-                  onClick={() => handleShowPassword()}
-                  className={styles.toggleShowPassword}
+                  className={styles.passwordWarning}
+                  style={
+                    password.length < 8
+                      ? { color: "#CFA75F" }
+                      : passwordRegex.test(password)
+                      ? { color: "#A7D8A2" }
+                      : { color: "lightcoral" }
+                  }
                 >
-                  {!showPassword ? (
-                    <FontAwesomeIcon icon={faEye} color="#CFA75F" size="lg" style={{cursor:"pointer"}}/>
-                  ) : (
-                    <FontAwesomeIcon
-                      icon={faEyeSlash}
-                      color="#CFA75F"
-                      size="lg"
-                      style={{cursor:"pointer"}}
-                    />
-                  )}
+                  Le mot de passe doit contenir 1 minuscule, 1 majuscule, 1
+                  chiffre, 1 caractère parmi #@$!%*?& et être d'au moins 8
+                  caractères
                 </div>
               </div>
-              <div
-                className={styles.passwordWarning}
-                style={
-                  password.length < 8
-                    ? { color: "#CFA75F" }
-                    : passwordRegex.test(password)
-                    ? { color: "#A7D8A2" }
-                    : { color: "lightcoral" }
+              <div className={styles.agreeConditionsCheckboxContainer}>
+                <div className={styles.agreeConditionsCheckbox}>
+                  <input
+                    type="checkbox"
+                    checked={isCheckedCGU}
+                    onChange={() => setIsCheckedCGU(!isCheckedCGU)}
+                  />{" "}
+                  <div className={styles.agreeConditionsText}>
+                    Accepter les CGU et la politique de confidentialité
+                  </div>
+                </div>
+                {displayWarningCGU && (
+                  <div className={styles.warningCGU}>
+                    Pour t'inscrire, il faut accepter les conditions
+                    d'utilisation du service !
+                  </div>
+                )}
+              </div>
+              <button
+                className={styles.modalSigninButton}
+                id="register"
+                onClick={() =>
+                  handleSignup(
+                    firstName,
+                    lastName,
+                    username,
+                    password,
+                    email,
+                    (connectionWithSocials = false)
+                  )
                 }
               >
-                Le mot de passe doit contenir 1 minuscule, 1 majuscule, 1
-                chiffre, 1 caractère parmi #@$!%*?& et être d'au moins 8
-                caractères
+                {" "}
+                Crée ton compte{" "}
+              </button>
+              <div
+                className={styles.switchSignup}
+                onClick={() => handleSwitchSignupClick()}
+              >
+                Déjà un compte ? Connecte-toi ici !
               </div>
             </div>
-            <div className={styles.agreeConditionsCheckboxContainer}>
-              <div className={styles.agreeConditionsCheckbox}>
-                <input
-                  type="checkbox"
-                  checked={isCheckedCGU}
-                  onChange={() => setIsCheckedCGU(!isCheckedCGU)}
-                />{" "}
-                <div className={styles.agreeConditionsText}>
-                  Accepter les CGU et la politique de confidentialité
-                </div>
-              </div>
-              {displayWarningCGU && (
-                <div className={styles.warningCGU}>
-                  Pour t'inscrire, il faut accepter les conditions d'utilisation
-                  du service !
-                </div>
-              )}
+            <div className={styles.connectsSeparator}>
+              <div className={styles.connectsSeparatorLine}></div>
+              <p className={styles.connectsSeparatorText}>OU</p>
+              <div className={styles.connectsSeparatorLine}></div>
             </div>
-            <button
-              className={styles.modalSigninButton}
-              id="register"
-              onClick={() => handleSignup()}
-            >
-              {" "}
-              Crée ton compte{" "}
-            </button>
-            <div
-              className={styles.switchSignup}
-              onClick={() => handleSwitchSignupClick()}
-            >
-              Déjà un compte ? Connecte-toi ici !
+            <div className={styles.connectsContainer}>
+              <GoogleLogin
+                className={styles.googleConnect}
+                onSuccess={(credentialResponse) => {
+                  let googleUserInfo = jwtDecode(credentialResponse.credential);
+                  console.log(jwtDecode(credentialResponse.credential));
+                  handleSignup(
+                    googleUserInfo.given_name,
+                    googleUserInfo.family_name,
+                    googleUserInfo.name,
+                    "",
+                    googleUserInfo.email,
+                    true
+                  );
+                }}
+              />
             </div>
+
             <div className={styles.errorMsg}> {msg} </div>
           </div>
         )}
@@ -325,68 +382,93 @@ function Login() {
                 Ton email et/ou ton mot de passe semblent incorrects
               </p>
             )}
-            <div className={styles.inputFieldsArea}>
-              <input
-                className={styles.loginField}
-                type="text"
-                placeholder="Email"
-                id="signInEmail"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-            </div>
-            <div className={styles.passwordArea}>
-              <div className={styles.passwordAreaInput}>
+            <div className={styles.traditionalSigninContainer}>
+              <div className={styles.inputFieldsArea}>
                 <input
-                  className={styles.loginFieldPassword}
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Mot de passe"
-                  id="signInPassword"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                  value={password}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleSignin();
-                    }
-                  }}
+                  className={styles.loginField}
+                  type="text"
+                  placeholder="Email"
+                  id="signInEmail"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                 />
+              </div>
+              <div className={styles.passwordArea}>
+                <div className={styles.passwordAreaInput}>
+                  <input
+                    className={styles.loginFieldPassword}
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Mot de passe"
+                    id="signInPassword"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    value={password}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        handleSignin(email, password, (connectionWithSocials = false));
+                      }
+                    }}
+                  />
+                  <div
+                    onClick={() => handleShowPassword()}
+                    className={styles.toggleShowPassword}
+                  >
+                    {!showPassword ? (
+                      <FontAwesomeIcon
+                        icon={faEye}
+                        color="#CFA75F"
+                        size="lg"
+                        style={{ cursor: "pointer" }}
+                      />
+                    ) : (
+                      <FontAwesomeIcon
+                        icon={faEyeSlash}
+                        color="#CFA75F"
+                        size="lg"
+                        style={{ cursor: "pointer" }}
+                      />
+                    )}
+                  </div>
+                </div>
                 <div
-                  onClick={() => handleShowPassword()}
-                  className={styles.toggleShowPassword}
+                  className={styles.forgotPassword}
+                  onClick={() => handleForgotPasswordClick()}
                 >
-                  {!showPassword ? (
-                    <FontAwesomeIcon icon={faEye} color="#CFA75F" size="lg" style={{cursor:"pointer"}} />
-                  ) : (
-                    <FontAwesomeIcon
-                      icon={faEyeSlash}
-                      color="#CFA75F"
-                      size="lg"
-                      style={{cursor:"pointer"}}
-                    />
-                  )}
+                  Mot de passe oublié ?
                 </div>
               </div>
-              <div
-                className={styles.forgotPassword}
-                onClick={() => handleForgotPasswordClick()}
+              <button
+                className={styles.modalSigninButton}
+                id="connection"
+                onClick={() =>
+                  handleSignin(email, password, (connectionWithSocials = false))
+                }
               >
-                Mot de passe oublié ?
+                Connexion
+              </button>
+              <div
+                className={styles.switchSignup}
+                onClick={() => handleSwitchSignupClick()}
+              >
+                Toujours pas de compte ? Sois collaboratif et crées-en un !
               </div>
             </div>
-            <button
-              className={styles.modalSigninButton}
-              id="connection"
-              onClick={() => handleSignin()}
-            >
-              Connexion
-            </button>
-            <div
-              className={styles.switchSignup}
-              onClick={() => handleSwitchSignupClick()}
-            >
-              Toujours pas de compte ? Sois collaboratif et crées-en un !
+            <div className={styles.connectsSeparator}>
+              <div className={styles.connectsSeparatorLine}></div>
+              <p className={styles.connectsSeparatorText}>OU</p>
+              <div className={styles.connectsSeparatorLine}></div>
+            </div>
+            <div className={styles.connectsContainer}>
+              <GoogleLogin
+                className={styles.googleConnect}
+                onSuccess={(credentialResponse) => {
+                  let googleUserInfo = jwtDecode(credentialResponse.credential);
+                  console.log(jwtDecode(credentialResponse.credential));
+
+                  handleSignin(googleUserInfo.email, "", true);
+                }}
+              />
             </div>
           </div>
         )}
