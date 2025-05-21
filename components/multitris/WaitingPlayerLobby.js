0764c7193bb3dc0509954f19e284092788e79a92
anguyen <nguyen.alexandre.i@gmail.com>
2025-05-21 16:45:14 +0200

    feat/PLAYITY-13 - add lobby and socker

--- components/multitris/WaitingPlayerLobby.js
+++ components/multitris/WaitingPlayerLobby.js
@@ -3,7 +3,7 @@ import styles from "../../styles/PlayerLobby.module.css";
 function WaitingPlayerLobby() {
   return (
     <div className={styles.playerLobbyContainer}>
-      <div style={styles.gameTitle}>...waiting</div>
+      <div style={styles.gameTitle}>waiting...</div>
     </div>
   );
 }
