435d30258fa3d44fe857312524477bf20bf54c26
Henri <henrilorilleux@gmail.com>
2025-05-28 09:32:16 +0200

    socket fin de partie + calcul des scores V2

--- pages/multitris/[code]/index.js
+++ pages/multitris/[code]/index.js
@@ -85,6 +85,7 @@ export default function LobbyPage() {
       body: JSON.stringify({ gameId: game._id, lobbyId: lobby._id})
     });
 
+    console.log('id du lobby : ', lobby._id);
     // La partie est créée en base, on récupère la réponse qui contient l'id de la partie
     const data = await response.json();
     console.log('réponse obtenue du back : ', data);
