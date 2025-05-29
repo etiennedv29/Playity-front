558782deeae5bfe36e43ec91340e268f8f5e1175
Etienne de V <edevalmont@gmail.com>
2025-05-29 10:03:52 +0200

    scores display but duplicated

--- components/Home.js
+++ components/Home.js
@@ -18,6 +18,7 @@ function Home() {
 
   const getAllGames = async () => {
     const response = await fetch('http://localhost:3000/games');
+    //const response = await fetch ('https://p01--playity-back--c9dy8yj49fkp.code.run/games')
     const data = await response.json();
     const otherGamesData = data.filter((e) => !e.tags.includes("topGame"))
     setGamesData(otherGamesData);
--- components/Slide.js
+++ components/Slide.js
@@ -3,26 +3,33 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 function Slide(props) {
+  const router = useRouter();
 
-    const router = useRouter();
+  function handleTopGame(gameName) {
+    //console.log('Click détecté')
+    gameName.toLowerCase().replace(" ", "");
+    router.push(`http://localhost:3001/${gameName}`);
+    //console.log('Voici le nom du jeu cliqué : ', gameName);
+  }
 
-    function handleTopGame(gameName) {
-        console.log('Click détecté')
-        gameName.toLowerCase().replace(" ", "");
-        router.push(`http://localhost:3001/${gameName}`);
-        console.log('Voici le nom du jeu cliqué : ', gameName);
-    }
-
-    return (
-        <div className={styles.slide}>
-            <div style={{position: "relative"}} onClick={() => handleTopGame(props.name)}>
-                <div className={styles.slideTitle}>{props.name}</div>
-                <Image className={styles.image} src={props.image} alt={props.name} width={300} height={200} />
-                <p className={styles.description}>{props.description}</p>
-            </div>
-
-        </div>
-    )
+  return (
+    <div className={styles.slide}>
+      <div
+        style={{ position: "relative" }}
+        onClick={() => handleTopGame(props.name)}
+      >
+        <div className={styles.slideTitle}>{props.name}</div>
+        <Image
+          className={styles.image}
+          src={props.image}
+          alt={props.name}
+          width={300}
+          height={200}
+        />
+        <p className={styles.description}>{props.description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Slide;
